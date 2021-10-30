a06f004263fb4db901f24a95ec4c372d00ca8b15
Liam park <hyungryulpark@Hyungui-MacBookPro.local>
2021-10-30 18:35:37 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { Image } from "react-bootstrap";
+import "./App.css";
+
+import NavbarBrand from "./NavbarBrand";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavbarBrand />
+      <section id="section-top">
+        <div id="main-top">
+          <div className="top-desc">
+            <h1 className="top-title">
+              회사의 주인은
+              <br />
+              당신
+            </h1>
+            <p className="text-m">
+              회사 경영에 대한 당신의 생각을 제안 해보세요.
+              <br />
+              다른 주주의 좋은 생각에 의결권을 위임 해보세요.
+            </p>
+          </div>
+          <div className="img-box"></div>
+        </div>
+      </section>
+
+      <footer class="text-muted py-5">
+        <div class="container">
+          <p class="float-end mb-1">
+            <a href="#">Back to top</a>
+          </p>
+          <p class="mb-1">
+            Album example is &copy; Bootstrap, but please download and customize
+            it for yourself!
+          </p>
+          <p class="mb-0">
+            New to Bootstrap? <a href="/">Visit the homepage</a> or read our{" "}
+            <a href="../getting-started/introduction/">getting started guide</a>
+            .
+          </p>
+        </div>
+      </footer>
+
+      <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
     </div>
   );
 }
---
+++ src/NavbarBrand/index.js
@@ -0,0 +1,15 @@
+const NavbarBrand = () => {
+  return (
+    <header>
+      <div id="fix-bar" class="navbar navbar-white bg-white">
+        <div class="container">
+          <div class="navbar-brand d-flex align-items-center">
+            <strong>AntHouse</strong>
+          </div>
+          <span class="navbar-text">제휴 문의</span>
+        </div>
+      </div>
+    </header>
+  );
+};
+export default NavbarBrand;
