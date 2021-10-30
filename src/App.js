630a8fd017c0c9589095d0497978a173d01def2a
Liam park <hyungryulpark@Hyungui-MacBookPro.local>
2021-10-30 18:46:43 +0900

    first

--- src/App.js
+++ src/App.js
@@ -26,9 +26,7 @@ function App() {
 
       <footer class="text-muted py-5">
         <div class="container">
-          <p class="float-end mb-1">
-            <a href="#">Back to top</a>
-          </p>
+          <p class="float-end mb-1">Back to top</p>
           <p class="mb-1">
             Album example is &copy; Bootstrap, but please download and customize
             it for yourself!
