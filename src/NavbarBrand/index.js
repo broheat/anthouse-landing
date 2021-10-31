a207e72bb08b52a57d47b12a19bd26047b51c6b2
Liam park <hyungryulpark@Hyungui-MacBookPro.local>
2021-10-31 16:53:21 +0900

    first

--- src/NavbarBrand/index.js
+++ src/NavbarBrand/index.js
@@ -4,7 +4,7 @@ const NavbarBrand = () => {
       <div id="fix-bar" class="navbar navbar-white bg-white">
         <div class="container">
           <div class="navbar-brand d-flex align-items-center">
-            <strong>AntHouse</strong>
+            <strong>HeyHolder</strong>
           </div>
           <span class="navbar-text">제휴 문의</span>
         </div>
