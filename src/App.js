7362d94506b54a7c3c4a1e91f9da5fca36947a41
Liam park <hyungryulpark@Hyungui-MacBookPro.local>
2021-11-01 08:51:22 +0900

    fix bootstrap

--- src/App.js
+++ src/App.js
@@ -5,38 +5,31 @@ function App() {
   return (
     <div>
       <NavbarBrand />
-      <div className="container-fluid">
-        <div className="row section justify-content-center align-items-center flex-wrap">
-          <div className="col-md-3">
-            <h1 className="top-title">
-              회사의 주인은
-              <br />
-              당신
-            </h1>
-            <p className="text-m">
-              회사 경영에 대한 당신의 생각을 제안 해보세요.
-              <br />
-              다른 주주의 좋은 생각에 의결권을 위임 해보세요.
-            </p>
+      <div className="page-header">
+        <div className="container">
+          <div className="row">
+            <div className="d-flex col-md-3 content justify-content-center align-items-center mx-auto">
+              <div>
+                <h1 className="top-title">
+                  회사의 주인은
+                  <br />
+                  당신
+                </h1>
+                <p className="text-m">
+                  회사 경영에 대한 당신의 생각을 제안 해보세요.
+                  <br />
+                  다른 주주의 좋은 생각에 의결권을 위임 해보세요.
+                </p>
+              </div>
+            </div>
+            <div className="d-flex col-md-3 content justify-content-center align-items-center mx-auto">
+              <div>
+                <img src={app01} className="col-md-2 h-50" alt="..." />
+              </div>
+            </div>
           </div>
-          <img src={app01} className="col-md-2 h-50" alt="..." />
         </div>
       </div>
-
-      <footer className="text-muted py-5">
-        <div className="container">
-          <p className="float-end mb-1">Back to top</p>
-          <p className="mb-1">
-            Album example is &copy; Bootstrap, but please download and customize
-            it for yourself!
-          </p>
-          <p className="mb-0">
-            New to Bootstrap? <a href="/">Visit the homepage</a> or read our{" "}
-            <a href="../getting-started/introduction/">getting started guide</a>
-            .
-          </p>
-        </div>
-      </footer>
 
       <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
     </div>
