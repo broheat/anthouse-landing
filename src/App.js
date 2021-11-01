621cd16095c7602ebb3651284bd8c31ce863cf26
Liam park <hyungryulpark@Hyungui-MacBookPro.local>
2021-11-01 20:54:54 +0900

    tage

--- src/App.js
+++ src/App.js
@@ -143,7 +143,11 @@ function App() {
         </div>
       </div>
 
-      <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+      <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"
+      ></script>
     </div>
   );
 }
