22d2cc26ea87eb18e42d2c096929c151d4a1c2c4
Liam park <hyungryulpark@Hyungui-MacBookPro.local>
2021-11-01 16:52:29 +0900

    회사 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import NavbarBrand from "./NavbarBrand";
 import app01 from "./imges/app01.png";
 import app02 from "./imges/app02.png";
+import company from "./imges/company.png";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <NavbarBrand />
       <div className="container-fluid main">
         <div className="row">
-          <div className="d-flex col-md-6 content">
+          <div className="d-flex col-lg-6 content">
             <div className="d-flex flex-column left">
               <h1 className="title">
                 회사의 주인은
@@ -22,7 +23,7 @@ function App() {
               </p>
             </div>
           </div>
-          <div className="d-flex col-md-6 content">
+          <div className="d-flex col-lg-6 content">
             <div className="right">
               <img src={app01} alt="주주제안" />
             </div>
@@ -31,13 +32,13 @@ function App() {
       </div>
       <div className="container-fluid second">
         <div className="row">
-          <div className="d-flex col-md-6 content">
+          <div className="d-flex col-lg-6 content">
             <div className="left-img">
               <img src={app02} alt="주주게시판" />
             </div>
           </div>
-          <div className="d-flex col-md-6 content">
-            <div>
+          <div className="d-flex col-lg-6 content">
+            <div className="d-flex flex-column">
               <h1 className="title">
                 주주들끼리만의
                 <br /> 소통 공간
@@ -48,6 +49,40 @@ function App() {
                 각종 광고와 허위 정보를 피하고 진짜 정보를 공유 해보세요.
               </p>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="container-fluid third">
+        <div className="row">
+          <div className="d-flex content justify-content-center">
+            <div className="d-flex flex-column">
+              <h1 className="title">HeyHolder를 가장 빠르게 만나보세요.</h1>
+              <p className="text-m">
+                주주 활동의 시작, HeyHolder 출시되면 첫 번째로 알려드릴께요!
+              </p>
+              <div className="d-flex">
+                <div className="col-5">
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="이메일로 신청하기"
+                  />
+                </div>
+                <div className="col-auto ms-2">
+                  <button type="submit" className="btn btn-primary" onclick="">
+                    신청하기
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container-fluid forth">
+        <div className="row">
+          <div className="d-flex justify-content-center company">
+            <img className="w-50" src={company} alt="협력업체" />
           </div>
         </div>
       </div>
